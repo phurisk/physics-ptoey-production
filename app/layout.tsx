82c11cfda4f649df006df8635fe531c9f38381c6
phurisk <phuris.k@kkumail.com>
2025-08-27 19:25:16 +0700

    landing_page/

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,9 @@ export const metadata: Metadata = {
   description: "เรียนฟิสิกส์กับครูพี่เต้ย ผู้เชี่ยวชาญด้านฟิสิกส์ที่มีประสบการณ์การสอนมากมาย พร้อมเทคนิคการสอนที่เข้าใจง่าย",
   keywords: "ฟิสิกส์, กวดวิชา, ครูพี่เต้ย, เรียนฟิสิกส์, GAT, PAT, ฟิสิกส์ม.ปลาย",
   generator: "physics-ptoey.app",
+  icons: {
+    icon: "/new-logo.png",
+  },
 }
 
 export default function RootLayout({
